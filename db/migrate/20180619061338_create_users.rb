376faac3f738ab14d58f9c2b88bb9f5a0564322f
Ienaga <ienaga@ijgn.jp>
2018-06-19 15:36:33 +0900

    mod migrate file

--- db/migrate/20180619061338_create_users.rb
+++ db/migrate/20180619061338_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :id
-      t.string :user_id
       t.string :password
       t.string :user_name
       t.string :mail_address
